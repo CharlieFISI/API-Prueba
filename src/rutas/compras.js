fe93d197b6cb7ee03c60134f584b87d3164ce2a2
LinuxChar <charlie.elizarbe@unmsm.edu.pe>
2023-01-01 06:56:18 -0500

    API v6.0

---
+++ src/rutas/compras.js
@@ -0,0 +1,62 @@
+const app = require("../app.js")
+const conexion = require("../conexion.js")
+//Compras
+app.get('/api/tienda_virtual/v1/compras', (req, res) => {
+    const sql = "SELECT * FROM compra"
+
+    conexion.query(sql, (e, results)=>{
+        if(e){
+            throw e
+        }
+        if(results.length > 0){
+            res.json(results)
+        }else{
+            res.send("Sin resultados")
+        }
+    })
+})
+app.get('/api/tienda_virtual/v1/compras/:id', (req, res) => {
+    const {id } = req.params
+    const sql = `SELECT * FROM compra WHERE idcliente = ${id}`
+    
+    conexion.query(sql, (e, results) => {
+        if(e){
+            throw e
+        }
+        if(results.length > 0){
+            res.json(results)
+        }else{
+            res.send("Sin resultados")
+        }
+    })
+})
+app.put('/api/tienda_virtual/v1/comprar', (req, res) => {
+    const sql = "INSERT INTO compra SET ?"
+    const productoObj = {
+        idcliente: req.body.idcliente,
+        Fecha: req.body.Fecha,
+        Tipo: req.body.Tipo
+    }
+
+    conexion.query(sql, productoObj, e => {
+        if(e){
+            throw e
+        }
+        res.send("Compra añadida")
+    })
+})
+app.put('/api/tienda_virtual/v1/comprar/planes', (req, res) => {
+    const sql = "INSERT INTO planesxcompra SET ?"
+    const productoObj = {
+        idPlan: req.body.idPlan,
+        idCompra: req.body.idCompra,
+        duracion: req.body.duracion
+    }
+
+    conexion.query(sql, productoObj, e => {
+        if(e){
+            throw e
+        }
+        res.send("Compra de plan añadida")
+    })
+})
--- src/rutas/planes.js
+++ src/rutas/planes.js
@@ -18,9 +18,24 @@ app.get('/api/tienda_virtual/v1/planes', (req, res) => {
 app.get('/api/visualizacion_plan/v1/planes', (req, res) => {
     //const {id } = req.params
     //const sql = `SELECT * FROM plan WHERE idPlan = ${id}`
-    const sql = `SELECT clientes.DNI, clientes.Nombre, planesxcliente.fechainicio, planesxcliente.fechafin, planesxcliente.estado, plan.NombrePlan FROM clientes RIGHT JOIN planesxcliente ON planesxcliente.idcliente = clientes.DNI LEFT JOIN planesxcompra ON planesxcompra.idplanesxcompra = planesxcliente.idplanescompra LEFT JOIN plan ON planesxcompra.idPlan = plan.idPlan`
+    const sql = `SELECT clientes.DNI, clientes.Nombre, planesxcliente.fechainicio, planesxcliente.fechafin, planesxcliente.estado, plan.NombrePlan FROM clientes RIGHT JOIN planesxcliente ON planesxcliente.idcliente = clientes.DNI LEFT JOIN planesxcompra ON planesxcompra.idplanesxcompra = planesxcliente.Fecha LEFT JOIN plan ON planesxcompra.idPlan = plan.idPlan`
 
     conexion.query(sql, (e, results) => {
+        if(e){
+            throw e
+        }
+        if(results.length > 0){
+            res.json(results)
+        }else{
+            res.send("Sin resultados")
+        }
+    })
+})
+app.get('/api/visualizacion_plan/v1/planes/fin/:id', (req, res) => {
+    const {id } = req.params
+    const sql = `SELECT fechafin FROM planesxcliente WHERE idcliente = ${id}`
+
+    conexion.query(sql, (e, results)=>{
         if(e){
             throw e
         }
